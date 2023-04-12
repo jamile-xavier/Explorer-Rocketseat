555337b83dbb026cdb6dc2ead2f4995821c16218
Jamile Mendonça <110774918+jamile-mendonca@users.noreply.github.com>
2023-04-12 12:17:32 -0300

    Add files via upload

--- Stage6/Projeto9 - SPA/js/index.js
+++ Stage6/Projeto9 - SPA/js/index.js
@@ -1,33 +1,12 @@
+import { Router } from './router.js'
 //mapeamento de rota
-const routes = {
-  "/": "/Stage6/Projeto9 - SPA/pages/home.html",
-  "/about": "/Stage6/Projeto9 - SPA/pages/about.html",
-  "/contact": "/Stage6/Projeto9 - SPA/pages/contact.html",
-  404: "/Stage6/Projeto9 - SPA/pages/404.html",
-}
+const router = new Router()
+router.add('/', "/pages/home.html")
+router.add("/about", "/pages/about.html")
+router.add("/contact", "/pages/contact.html")
+router.add(404, "/pages/404.html")
 
-function route(event) {
-  event = event || window.event
-  event.preventDefault()
+router.handle()
 
-  window.history.pushState({}, "", event.target.href)
-
-  handle()
-}
-
-function handle() {
-  //const pathname = window.location.pathname
-  const { pathname } = window.location
-  const route = routes[pathname] || routes[404]
-  //Fetch - Busca a rota (promises) / Then - quando concluir execute
-  fetch(route)
-    .then(data => data.text())
-    .then(html => {
-      document.querySelector('#app').innerHTML = html
-    })
-}
-
-handle()
-
-window.onpopstate = () => handle()
-window.route = () => route()
+window.onpopstate = () => router.handle()
+window.route = () => router.route()
--- Stage6/Projeto9 - SPA/js/router.js
+++ Stage6/Projeto9 - SPA/js/router.js
@@ -1,3 +1,29 @@
-class Router { }
+export class Router {
+  routes = {}
 
-const router = new Router()
+  add(routeName, page) {
+    this.routes[routeName] = page
+  }
+
+  route(event) {
+    event = event || window.event
+    event.preventDefault()
+
+    window.history.pushState({}, "", event.target.href)
+
+    this.handle()
+  }
+
+  handle() {
+    //const pathname = window.location.pathname
+    const { pathname } = window.location
+    const route = this.routes[pathname] || this.routes[404]
+    //Fetch - Busca a rota (promises) / Then - quando concluir execute
+    fetch(route)
+      .then(data => data.text())
+      .then(html => {
+        document.querySelector('#app').innerHTML = html
+      })
+  }
+
+}
