8b807868eb15bdc58bc47182053dc07e0fc4c23d
Jamile Mendonça <110774918+jamile-mendonca@users.noreply.github.com>
2023-04-28 08:51:12 -0300

    Add files via upload

---
+++ Stage8/Projeto11-API/src/routes/users.routes.js
@@ -0,0 +1,26 @@
+const { Router } = require("express");
+
+const UsersController = require("../controllers/UsersController");
+
+const usersRoutes = Router();
+
+/*function myMiddleware(request, response, next) {
+  console.log("Você passou pelo Middleware!");
+
+  if (!request.body.isAdmin) {
+    return response.json({ message: "user unauthorized" })
+  
+
+  next();
+}
+}*/
+
+
+const usersController = new UsersController()
+
+/*usersRoutes.use(myMiddleware);*/
+
+usersRoutes.post("/", /*myMiddleware,*/ usersController.create);
+usersRoutes.put("/:id", usersController.update);
+
+module.exports = usersRoutes;
