4abf9db8d795438c86d466c1c86464ca5a231757
Jamile Mendonça <110774918+jamile-mendonca@users.noreply.github.com>
2023-05-07 11:55:36 -0300

    Add files via upload

---
+++ Intensivão-Stage 1 a 4/Exercícios Js/script.js
@@ -0,0 +1,76 @@
+//1- Crie um script que exiba a mensagem "Hello World!" em um alerta no navegador.,
+
+//alert("Hello World!");
+
+//2- Crie um script que declare duas variáveis e exiba o resultado da soma entre elas.
+
+/*let number1 = prompt("Digite o número 1:");
+let number2 = prompt("Digite o número 2:");
+let soma = alert(Number(number1) + Number(number2));*/
+
+/*3- Crie um script que declare uma variável e verifique se o seu valor é um número.
+ Se for, exiba a mensagem "É um número", caso contrário, exiba a mensagem "Não é um número".*/
+
+/*let isANumber = 2;
+if (typeof isANumber == "number") {
+  alert("É um número")
+} else {
+  alert("Não é um número");
+}*/
+
+/*4- Crie um script que declare uma variável e verifique se o seu valor é uma string. Se for, exiba a mensagem
+ "É uma string", caso contrário, exiba a mensagem "Não é uma string".*/
+
+/*let isAString = "teste";
+
+if (typeof isAString == "string") {
+  alert("É uma string")
+} else {
+  alert("Não é uma string")
+} */
+
+/* 5- Crie um script que declare uma variável e verifique se o seu valor é um booleano.
+Se for, exiba a mensagem "É um booleano", caso contrário, exiba a mensagem "Não é um booleano".*/
+
+/*let isABoolean = false;
+if (typeof isABoolean == "boolean") {
+  alert("É um booleano")
+} else {
+  alert("Não é um booleano")
+} */
+
+//6- Crie um script que declare duas variáveis e exiba o resultado da subtração entre elas.
+
+/*let number1 = prompt("Digite o número 1:");
+let number2 = prompt("Digite o número 2:");
+let subtracao = alert(Number(number1) - Number(number2))*/
+
+// 7- Crie um script que declare duas variáveis e exiba o resultado da multiplicação entre elas.
+/*let number1 = prompt("Digite o número 1:");
+let number2 = prompt("Digite o número 2:");
+let multiplicacao = alert(Number(number1) * 4 Number(number2))*/
+
+// 8-Crie um script que declare duas variáveis e exiba o resultado da divisão entre elas.
+
+/*let number1 = prompt("Digite o número 1:");
+let number2 = prompt("Digite o número 2:");
+let divisao = alert(Number(number1) - Number(number2))*/
+
+/* 9- Crie um script que declare uma variável e verifique se o seu valor é um número par. Se for, exiba a mensagem
+"É um número par", caso contrário, exiba a mensagem "Não é um número par".*/
+/*let isNumberPar = Number(prompt("Digite o número:"));
+if (isNumberPar % 2 == 0) {
+  alert("É um número par")
+} else {
+  alert("Não é um número par")
+}*/
+
+
+/* 10- Crie um script que declare uma variável e verifique se o seu valor é um número ímpar. Se for, exiba a mensagem 
+"É um número ímpar", caso contrário, exiba a mensagem "Não é um número ímpar".*/
+let isNumberImpar = Number(prompt("Digite o número:"));
+if (isNumberImpar % 2 != 0) {
+  alert("É um número ímpar")
+} else {
+  alert("Não é um número ím4par")
+}
