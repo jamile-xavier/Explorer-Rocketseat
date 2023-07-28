9df8a4bc0374b32fb4074767f6657d5a0e7b7332
Jamile Xavier <110774918+jamile-xavier@users.noreply.github.com>
2023-07-28 17:05:03 -0300

    Delete Stage9-Projeto12-FrontEnd_RocketNotes directory

--- Stage9-Projeto12-FrontEnd_RocketNotes/src/components/Tag/index.jsx
+++
@@ -1,5 +0,0 @@
-import { Container } from './style';
-
-export function Tag({ title, ...rest }) {
-  return <Container {...rest}>{title}</Container>;
-}
--- Stage9-Projeto12-FrontEnd_RocketNotes/src/components/Textarea/index.jsx
+++
@@ -1,5 +0,0 @@
-import { Container } from './style';
-
-export function TextArea({ value, ...rest }) {
-  return <Container {...rest}>{value}</Container>;
-}
